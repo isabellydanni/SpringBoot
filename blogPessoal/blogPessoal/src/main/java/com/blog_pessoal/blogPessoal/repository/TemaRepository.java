59c501faff5f5132131298d7a6142d6b6a50917c
isabellydanni <isabellydanni@outlook.com>
2020-09-14 11:07:37 -0300

    exercícíos

--- blogPessoal/blogPessoal/src/main/java/com/blog_pessoal/blogPessoal/repository/TemaRepository.java
+++ blogPessoal/blogPessoal/src/main/java/com/blog_pessoal/blogPessoal/repository/TemaRepository.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import com.blog_pessoal.blogPessoal.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long>{
 
 	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
